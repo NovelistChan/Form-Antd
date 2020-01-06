45d8e16251614455451ab0c4f05b3b09b3a311af
perishcode@gmail.com <perishcode@gmail.com>
2020-01-06 16:23:05 +0800

    update

--- src/components/form/components/fields/ObjectField.js
+++ src/components/form/components/fields/ObjectField.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import utils from '../utils'
-import Package from '../Package'
+import React from "react";
+import utils from "../utils";
+import Package from "../Package";
 
-export default function (props) {
-
-  const { schema, formData, global, onChange } = props
+export default function(props) {
+  const { schema, formData, global, onChange } = props;
 
   return (
-    <div className="field object" >
+    <div className={`field object ${schema.layout || "col"}`}>
       {Object.keys(schema.properties).map(name => {
-        const subSchema = schema.properties[name]
-        // if(subSchema.type==="date"){
-        //   console.log(subSchema)
-        // }
+        const subSchema = schema.properties[name];
         return (
           <Package
             key={name}
             schema={subSchema}
             global={global}
-            formData={formData[name] || subSchema.default || utils.default.parser(subSchema.type)}
+            formData={
+              formData[name] ||
+              subSchema.default ||
+              utils.default.parser(subSchema.type)
+            }
             onChange={data => {
-              let value = { ...formData }
-              value[name] = data
-              onChange(value)
-            }} />
-        )
+              let value = { ...formData };
+              value[name] = data;
+              onChange(value);
+            }}
+          />
+        );
       })}
     </div>
-  )
+  );
 }
